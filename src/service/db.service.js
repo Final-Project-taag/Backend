10d79ed3bb870f7f20ae72a48870b5ee9a7362a9
ammar arnous <ammar.arnous@dci-student.de>
2023-05-02 14:48:27 +0200

    merge gaby

--- src/service/db.service.js
+++ src/service/db.service.js
@@ -2,24 +2,25 @@ import mongoose from "mongoose";
 
 
 export async function connectToDb(callback) {
-    try {
-        // Setze den 'strict' Mode fuer mongoose (Felder, die nicht im Schema enthalten sind, werden nicht mitgespeichert)
-        mongoose.set('strictQuery', true);
+try {
+// Setze den 'strict' Mode fuer mongoose (Felder, die nicht im Schema enthalten sind, werden nicht mitgespeichert)
+mongoose.set('strictQuery', true);
 
-        await mongoose.connect(`mongodb://${process.env.DB_HOST}/${process.env.DB_NAME}`, {
-            maxPoolSize: 10
-        });
+await mongoose.connect(process.env.MONGODB_URI, {
+useNewUrlParser: true,
+useUnifiedTopology: true,
+maxPoolSize: 10
+});
 
-        // Falls callback direkt nach Verbindung ausgefuehrt werden soll
-        // Bspw. ein Seeding von intialen DB Eintraegen
-        if (callback) {
-            // fuehre callback aus
-            callback();
-        }
-
-        console.log('Connection to DB established');
-
-    } catch (error) {
-        console.error(error);
-    }
+// Falls callback direkt nach Verbindung ausgefuehrt werden soll
+// Bspw. ein Seeding von intialen DB Eintraegen
+if (callback) {
+// fuehre callback aus
+callback();
 }
+
+console.log('Verbindung zu MongoDB Atlas hergestellt');
+} catch (error) {
+console.error(error);
+}
+} 
